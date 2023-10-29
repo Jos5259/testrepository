82b0d3efeb145ab070f0443c5d2f733e7793f4df
Jos5259 <98203966+Jos5259@users.noreply.github.com>
2023-10-29 16:48:12 +0000

    links attempt 2

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("home.html")
+
+@app.route('/select')
+def select():
+  return render_template("select.html")
+
+@app.route('/versus')
+def versus():
+  return render_template("versus.html")
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
